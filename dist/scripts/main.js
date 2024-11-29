1eb820b3731cca6998de7204a5277965dd77635e
Wallace Piovani <whspiovani@gmail.com>
2024-11-29 14:32:33 -0300

    criado e configurada contagem regressiva da landing page

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -586,6 +586,25 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"3cYfC":[function(require,module,exports,__globalThis) {
 AOS.init();
+const dataDoEvento = new Date("Dec 25, 2025 18:00:00");
+const timestampDoEvento = dataDoEvento.getTime();
+const countdown = setInterval(function() {
+    const now = new Date();
+    const timeStampAtual = now.getTime();
+    const timeToTheEvent = timestampDoEvento - timeStampAtual;
+    const diasEmMs = 86400000;
+    const horasEmMs = 3600000;
+    const minutosEmMs = 60000;
+    const daysToTheEvent = Math.floor(timeToTheEvent / diasEmMs);
+    const hoursToTheEvent = Math.floor(timeToTheEvent % diasEmMs / horasEmMs);
+    const minutesToTheEvent = Math.floor(timeToTheEvent % horasEmMs / minutosEmMs);
+    const secondsToTheEvent = Math.floor(timeToTheEvent % minutosEmMs / 1000);
+    document.getElementById('countdown').innerHTML = `${daysToTheEvent}D ${hoursToTheEvent}H ${minutesToTheEvent}M ${secondsToTheEvent}S`;
+    if (timeToTheEvent < 0) {
+        clearInterval(countdown);
+        document.getElementById('countdown').innerHTML = 'Evento expirado.';
+    }
+}, 1000);
 
 },{}]},["3PCYQ","3cYfC"], "3cYfC", "parcelRequire94c2")
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,31 @@
 AOS.init();
+
+const dataDoEvento = new Date("Dec 25, 2025 18:00:00");
+const timestampDoEvento = dataDoEvento.getTime();
+
+const countdown = setInterval(function() {
+    const now = new Date();
+    const timeStampAtual = now.getTime();
+
+    const timeToTheEvent = timestampDoEvento - timeStampAtual;
+
+    const diasEmMs= 1000 * 60 * 60 * 24;
+    const horasEmMs= 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
+
+
+    const daysToTheEvent = Math.floor(timeToTheEvent / diasEmMs);
+    const hoursToTheEvent = Math.floor((timeToTheEvent % diasEmMs) / horasEmMs);
+    const minutesToTheEvent = Math.floor((timeToTheEvent % horasEmMs) / minutosEmMs);
+    const secondsToTheEvent = Math.floor((timeToTheEvent % minutosEmMs) / 1000);
+
+
+    document.getElementById('countdown').innerHTML =  `${daysToTheEvent}D ${hoursToTheEvent}H ${minutesToTheEvent}M ${secondsToTheEvent}S`;
+
+    if (timeToTheEvent < 0){
+        clearInterval(countdown);
+        document.getElementById('countdown').innerHTML = 'Evento expirado.';
+    }
+
+}, 1000);
+
